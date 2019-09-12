571ab4b8494a1005fd505f832571ccd5aecca545
ChristianMac <christiansmac@hotmail.com>
2019-09-12 13:15:46 +0200

    ITemSensor og FakeTempSensor

---
+++ ECS Legacy Solution/ECS.Refactored/FakeTempSensor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ECS.Refactored
+{
+    internal class FakeTempSensor
+    {
+        private Random Fakegen = new Random();
+
+        public int FakeGetTemp()
+        {
+            return Fakegen.Next(-5, 45);
+        }
+
+        public bool FakeRunSelfTest()
+        {
+            return true;
+        }
+    }
+}
---
+++ ECS Legacy Solution/ECS.Refactored/ITempSensor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECS.Refactored
+{
+    public interface ITempSensor
+    {
+        Random Gen();
+        int GetTemp();
+        bool RunSelfTest();
+    }
+}
